cad0079d0130452dfde94ca2cd2d10680eadc4e9
Danila Kovtun <danila.kovtunn@gmail.com>
2023-11-22 21:54:30 +0300

    PAR-73 add p0f rawParser and Quirks skeleton

--- p0f.go
+++ p0f.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
+	"strconv"
+	"strings"
 )
 
 type IPVersion int
@@ -45,7 +47,7 @@ type P0FSignature struct {
 	// TCP layer.
 
 	MSS     int                    // Maximum segment size, if specified in TCP options. '*' == -1.
-	WSize   uint16                 // Window size. TODO: '*', 'mss*4', '%8192'
+	WSize   int                    // Window size. '*' == -1, 'mss*4' == -2, 'mtu*4' == -3, '%8192' == -4
 	Scale   int                    // Window scaling factor, if specified in TCP options. '*' == -1
 	OLayout []layers.TCPOptionKind // Layout and ordering of TCP options, if any.
 	Quirks  []Quirk                // Properties and quirks observed in IP or TCP headers.
@@ -54,6 +56,131 @@ type P0FSignature struct {
 
 // ParseRawSig creates new P0FSignature from raw_sig p0f record.
 func ParseRawSig(rawSig string) (P0FSignature, error) {
+	// example - 4:64+0:0:1460:65535,9:mss,sok,ts,nop,ws:df,id+:0
+	signatures := strings.Split(rawSig, ":")
+	p0f := P0FSignature{}
+	sigAliases := []string{"ver", "ittl", "olen", "mss", "wsize", "olayout", "quirks", "pclass"}
+	for i, sigAlias := range sigAliases {
+		switch sigAlias {
+		case "ver":
+			num, err := strconv.Atoi(signatures[i])
+			if err != nil {
+				return P0FSignature{}, fmt.Errorf("unknown version: %s", err)
+			}
+			p0f.Ver = IPVersion(num)
+		case "ittl":
+			sum := strings.Split(signatures[i], "+")
+			ittl, err := strconv.Atoi(sum[0])
+			if err != nil {
+				return P0FSignature{}, fmt.Errorf("unknown ittl: %s", err)
+			}
+			if len(sum) == 2 {
+				secondPath, err := strconv.Atoi(sum[1])
+				if !(err != nil) {
+					ittl += secondPath
+				}
+			}
+			p0f.ITTL = uint8(ittl)
+		case "olen":
+			olen, _ := strconv.Atoi(signatures[i])
+			// ignore errors
+			p0f.OLen = olen
+		case "mss":
+			mss, err := strconv.Atoi(signatures[i])
+			if err != nil {
+				// capture mss="*"
+				mss = -1
+			}
+			p0f.MSS = mss
+		case "wsize":
+			var (
+				wsize, scale int
+				err          error
+			)
+			wSizeAndScale := strings.Split(signatures[i], ",")
+			switch wSizeAndScale[0] {
+			case "mss*4":
+				p0f.WSize = -2
+			case "mtu*4":
+				p0f.WSize = -3
+			case "%8192":
+				p0f.WSize = -4
+			case "*":
+				p0f.WSize = -1
+			default:
+				wsize, err = strconv.Atoi(wSizeAndScale[0])
+				if err != nil {
+					return P0FSignature{}, fmt.Errorf("unknown wsize: %s", err)
+				}
+			}
+			if wSizeAndScale[1] == "*" {
+				scale = -1
+			}
+			scale, err = strconv.Atoi(wSizeAndScale[1])
+			if err != nil {
+				return P0FSignature{}, fmt.Errorf("unknown scale: %s", err)
+			}
+			p0f.WSize = wsize
+			p0f.Scale = scale
+		case "olayout":
+			olayout := make([]layers.TCPOptionKind, 0, 8)
+			lst := strings.Split(signatures[i], ",")
+			layouts := map[string]layers.TCPOptionKind{
+				"eol": layers.TCPOptionKindEndList,
+				"nop": layers.TCPOptionKindNop,
+				"mss": layers.TCPOptionKindMSS,
+				"ws":  layers.TCPOptionKindWindowScale,
+				"sok": layers.TCPOptionKindSACKPermitted,
+				"ts":  layers.TCPOptionKindTimestamps,
+			}
+			for _, layout := range lst {
+				if strings.HasPrefix(layout, "eol+") {
+					padStr := strings.TrimPrefix(layout, "eol+")
+					pad, err := strconv.Atoi(padStr)
+					if err != nil {
+						return P0FSignature{}, fmt.Errorf("unknown scale: %s", err)
+					}
+					olayout = append(olayout, layers.TCPOptionKind(pad*255))
+					continue
+				}
+				olayout = append(olayout, layouts[layout])
+			}
+			p0f.OLayout = olayout
+		case "quirks":
+			lst := strings.Split(signatures[i], ",")
+			quirkMap := map[string]Quirk{
+				"df":     dfQuirk{},
+				"id+":    idpQuirk{},
+				"id-":    idmQuirk{},
+				"ecn":    ecnQuirk{},
+				"0+":     zeropQuirk{},
+				"flow":   flowQuirk{},
+				"seq-":   seqmQuirk{},
+				"ack+":   ackpQuirk{},
+				"ack-":   ackmQuirk{},
+				"uptr+":  uptrpQuirk{},
+				"urgf+":  urgfpQuirk{},
+				"pushf+": pushfpQuirk{},
+				"ts1-":   ts1mQuirk{},
+				"ts2+":   ts2pQuirk{},
+				"opt+":   optpQuirk{},
+				"exws":   exwsQuirk{},
+				"bad":    badQuirk{},
+			}
+			for _, quirk := range lst {
+				p0f.Quirks = append(p0f.Quirks, quirkMap[quirk])
+			}
+		case "pclass":
+			switch signatures[i] {
+			case "0":
+				p0f.PClass = 0
+			case "*":
+				p0f.PClass = -1
+			case "+":
+				p0f.PClass = 1
+			}
+		}
+	}
 	panic("not implemented")
 }
 
@@ -69,8 +196,6 @@ var iosFP = P0FSignature{
 		layers.TCPOptionKindMSS,                           //"mss",
 		layers.TCPOptionKindNop,                           //"nop",
 		layers.TCPOptionKindWindowScale,                   //"ws",
-		layers.TCPOptionKindNop,                           //"nop",
-		layers.TCPOptionKindNop,                           //"nop",
 		layers.TCPOptionKindTimestamps,                    //"ts",
 		layers.TCPOptionKindSACKPermitted,                 //"sok",
 		layers.TCPOptionKindEndList, TCPOptionPaddingByte, //"eol+1",
--- quirks.go
+++ quirks.go
@@ -31,3 +31,163 @@ func (q dfQuirk) Apply(packet gopacket.Packet) error {
 	ip.Flags |= layers.IPv4DontFragment
 	return nil
 }
+
+type idpQuirk struct{}
+
+func (q idpQuirk) Name() string {
+	return "id+"
+}
+
+func (q idpQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type idmQuirk struct{}
+
+func (q idmQuirk) Name() string {
+	return "id-"
+}
+
+func (q idmQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type ecnQuirk struct{}
+
+func (q ecnQuirk) Name() string {
+	return "ecn+"
+}
+
+func (q ecnQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type zeropQuirk struct{}
+
+func (q zeropQuirk) Name() string {
+	return "0+"
+}
+
+func (q zeropQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type flowQuirk struct{}
+
+func (q flowQuirk) Name() string {
+	return "flow"
+}
+
+func (q flowQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type seqmQuirk struct{}
+
+func (q seqmQuirk) Name() string {
+	return "seq-"
+}
+
+func (q seqmQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type ackpQuirk struct{}
+
+func (q ackpQuirk) Name() string {
+	return "ack+"
+}
+
+func (q ackpQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type ackmQuirk struct{}
+
+func (q ackmQuirk) Name() string {
+	return "ack-"
+}
+
+func (q ackmQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type uptrpQuirk struct{}
+
+func (q uptrpQuirk) Name() string {
+	return "uptr+"
+}
+
+func (q uptrpQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type urgfpQuirk struct{}
+
+func (q urgfpQuirk) Name() string {
+	return "urgf+"
+}
+
+func (q urgfpQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type pushfpQuirk struct{}
+
+func (q pushfpQuirk) Name() string {
+	return "pushf+"
+}
+
+func (q pushfpQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type ts1mQuirk struct{}
+
+func (q ts1mQuirk) Name() string {
+	return "ts1-"
+}
+
+func (q ts1mQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type ts2pQuirk struct{}
+
+func (q ts2pQuirk) Name() string {
+	return "ts2+"
+}
+
+func (q ts2pQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type optpQuirk struct{}
+
+func (q optpQuirk) Name() string {
+	return "opt+"
+}
+
+func (q optpQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type exwsQuirk struct{}
+
+func (q exwsQuirk) Name() string {
+	return "exws"
+}
+
+func (q exwsQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
+
+type badQuirk struct{}
+
+func (q badQuirk) Name() string {
+	return "bad"
+}
+
+func (q badQuirk) Apply(packet gopacket.Packet) error {
+	return nil
+}
